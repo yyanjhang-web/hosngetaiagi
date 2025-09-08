77c3da60d89f491477e3140bf6d82a38708e418e
yyanjhang-web <yyanjhang@gmail.com>
2025-09-08 21:19:34 +0800

    Initial game upload

---
+++ images.js
@@ -0,0 +1,8 @@
+const images = [
+    '1-1.png', '1-2.png', '1-3.png', '1-4.png', '1-5.png', '1-6.png',
+    '2-1.png', '2-2.png', '2-3.png', '2-4.png', '2-5.png', '2-6.png',
+    '3-1.png', '3-2.png', '3-3.png', '3-4.png', '3-5.png', '3-6.png',
+    '4-1.png', '4-2.png', '4-3.png', '4-4.png', '4-5.png', '4-6.png',
+    '5-1.png', '5-2.png', '5-3.png', '5-4.png', '5-5.png', '5-6.png',
+    '6-1.png'
+];
